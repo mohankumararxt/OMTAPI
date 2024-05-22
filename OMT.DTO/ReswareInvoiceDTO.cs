a9f1b26b6db9c5b6ec4ccd2a323c98f3a0d09c7e
Nivedhita <nivedhita.p@arxtlabs.com>
2024-05-22 17:32:46 +0530

    changes in invoice api

--- OMT.DTO/ReswareInvoiceDTO.cs
+++ OMT.DTO/ReswareInvoiceDTO.cs
@@ -2,7 +2,6 @@
 {
     public class ReswareInvoiceDTO
     {
-        public int InvoiceDumpId { get; set; }
         public string SystemOfRecord { get; set; }
         public string SkillSet { get; set; }
         public string BusinessGroup { get; set; }
