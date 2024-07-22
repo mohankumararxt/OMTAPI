e498a16e2382989161ec31ce72ebdef5f6a85078
Nivedhita <nivedhita.p@arxtlabs.com>
2024-07-22 16:59:14 +0530

    remove skillsetid from invoice joint sci update api input

--- OMT.DTO/InvoiceJointSciResponseDTO.cs
+++ OMT.DTO/InvoiceJointSciResponseDTO.cs
@@ -3,7 +3,6 @@
     public class InvoiceJointSciResponseDTO
     {
         public int InvoiceJointSciId { get; set; }
-        public int SkillSetId { get; set; }
         public int BusinessGroupId { get; set; }
         public int CustomerId { get; set; } 
         public int BusinessId { get; set; }
