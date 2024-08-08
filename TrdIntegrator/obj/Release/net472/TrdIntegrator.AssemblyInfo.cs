827f556baf8d87db952612e9b408db6005e1d6b1
Nivedhita <nivedhita.p@arxtlabs.com>
2024-08-08 19:39:26 +0530

    trd changes

--- TrdIntegrator/obj/Release/net472/TrdIntegrator.AssemblyInfo.cs
+++ TrdIntegrator/obj/Release/net472/TrdIntegrator.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrdIntegrator")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+712279aec4396a06cf9e5e1b18505d805cfbd308")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrdIntegrator")]
