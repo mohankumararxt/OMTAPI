71cd53c48019b9737c150275f14e4baebdb375b7
Nivedhita <nivedhita.p@arxtlabs.com>
2024-08-29 18:42:58 +0530

    merging from stg to prod

--- PendingSciOrders/obj/Release/net472/PendingSciOrders.AssemblyInfo.cs
+++ PendingSciOrders/obj/Release/net472/PendingSciOrders.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PendingSciOrders")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf0dabb42c3a51ddec18f9cb2c275d37dac3071")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1ed9939430e9d8d2b7cdc5568ec5d92aaa2656")]
 [assembly: System.Reflection.AssemblyProductAttribute("PendingSciOrders")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PendingSciOrders")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TrdIntegrator/obj/Release/net472/TrdIntegrator.AssemblyInfo.cs
+++ TrdIntegrator/obj/Release/net472/TrdIntegrator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrdIntegrator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf0dabb42c3a51ddec18f9cb2c275d37dac3071")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1ed9939430e9d8d2b7cdc5568ec5d92aaa2656")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrdIntegrator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrdIntegrator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
