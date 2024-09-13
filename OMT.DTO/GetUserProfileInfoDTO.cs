6a12f486d742f0b0ebd766da961a6f34743c8fee
Sowmeyaa <Sowmeyaa.M@arxtlabs.com>
2024-09-13 17:35:55 +0530

    Added UpdatePasswordByHR Api

--- OMT.DTO/GetUserProfileInfoDTO.cs
+++ OMT.DTO/GetUserProfileInfoDTO.cs
@@ -10,17 +10,17 @@ namespace OMT.DTO
     {
 
         public int UserId { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
         public bool IsHardStateUser { get; set; }
         public string EmployeeId { get; set; }  
         public int OrganizationId { get; set; }
-        public string? OrganizationName { get; set; }
-        public string? Mobile { get; set; }
-        public string? Password { get; set; }
-        public int? RoleId { get; set; }
-        public string? RoleName { get; set; }
+        public string OrganizationName { get; set; }
+        public string Mobile { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
     }
 }
