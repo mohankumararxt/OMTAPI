320fabcb073dc47ca52d103bd0ba1f9d3727e8a0
Nivedhita <nivedhita.p@arxtlabs.com>
2024-12-05 15:29:27 +0530

    changes in scipending webjob to put System-Pending as status

--- PendingSciOrders_5pm/Program.cs
+++ PendingSciOrders_5pm/Program.cs
@@ -61,7 +61,7 @@ namespace PendingSciOrders_5pm
                                             INNER JOIN SciPendingStatusSkillsets SPS ON SPS.SkillSetId = SS.SkillSetId
                                             INNER JOIN ProcessStatus PS ON PS.SystemofRecordId = SS.SystemofRecordId
                                             INNER JOIN TemplateColumns TC ON TC.SkillSetId = SS.SkillSetId
-                                            WHERE SS.isactive = 1 AND PS.Status = 'Pending'  AND SPS.IsActive =1 AND SPS.Scheduled_Time = '05:00 PM' AND SPS.Scheduled_Days = 'Sat'
+                                            WHERE SS.isactive = 1 AND PS.Status = 'System-Pending'  AND SPS.IsActive =1 AND SPS.Scheduled_Time = '05:00 PM' AND SPS.Scheduled_Days = 'Sat'
                                             ORDER BY SkillSetId";
 
                     SqlCommand GetSkillsets = new SqlCommand(SciSkillsets, connection);
