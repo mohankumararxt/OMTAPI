a50efb460a6dadb6a93b979040536de43432fc41
Nivedhita <nivedhita.p@arxtlabs.com>
2024-12-09 15:31:47 +0530

    added UpdateOrderStatusList api

--- OMT.DataService/Service/ProcessStatusService.cs
+++ OMT.DataService/Service/ProcessStatusService.cs
@@ -43,5 +43,42 @@ namespace OMT.DataService.Service
             }
             return resultDTO;
         }
+
+        public ResultDTO UpdateOrderStatusList(int systemofrecordid)
+        {
+            ResultDTO resultDTO = new ResultDTO() { IsSuccess = true, StatusCode = "200" };
+            try
+            {
+                
+                List<ProcessStatusResponseDTO> listofstatus = (from sor in _oMTDataContext.SystemofRecord
+                                                               join ps in _oMTDataContext.ProcessStatus on sor.SystemofRecordId equals ps.SystemOfRecordId
+                                                               where ps.IsActive == true && ps.SystemOfRecordId == systemofrecordid
+                                                               orderby ps.Status
+                                                               select new ProcessStatusResponseDTO
+                                                               {
+                                                                   SystemofRecordName = sor.SystemofRecordName,
+                                                                   Status = ps.Status,
+                                                                   SystemofRecordId = ps.SystemOfRecordId,
+                                                                   StatusId = ps.Id
+                                                               }).ToList();
+
+                if (listofstatus.Any(status => status.Status == "System-Pending"))
+                {
+                    listofstatus.RemoveAll(status => status.Status == "System-Pending");
+                }
+
+                resultDTO.IsSuccess = true;
+                resultDTO.Message = "List of Status";
+                resultDTO.Data = listofstatus;
+
+            }
+            catch (Exception ex)
+            {
+                resultDTO.IsSuccess = false;
+                resultDTO.StatusCode = "500";
+                resultDTO.Message = ex.Message;
+            }
+            return resultDTO;
+        }
     }
 }
