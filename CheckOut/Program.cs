005dcb1e994718b1250432abe0dc8f070b65e8b7
Nivedhita <nivedhita.p@arxtlabs.com>
2025-05-19 13:35:13 +0530

    added checkout webjob after 12 hrs

---
+++ CheckOut/Program.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckOut
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    internal class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var config = new JobHostConfiguration();
+
+            if (config.IsDevelopment)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            // call method to checkout all users if not checked out
+            CheckOut_users();
+        }
+
+        public class EmailDetails
+        {
+            public List<string> ToEmailIds { get; set; }
+            public string Subject { get; set; }
+            public string Body { get; set; }
+        }
+
+        public static void CheckOut_users()
+        {
+            string EmailUrl = "";
+            EmailUrl = ConfigurationManager.AppSettings["SendEmailUrl"];
+
+            try
+            {
+
+                string connectionString = ConfigurationManager.ConnectionStrings["DbConnectionString"].ConnectionString;
+
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Update users who checked in but didn’t check out after 12 hours
+                    string updateQuery = @"
+                                          UPDATE User_Checkin
+                                          SET CheckOut = GETUTCDATE()
+                                          WHERE CheckOut IS NULL 
+                                          AND CheckIn IS NOT NULL 
+                                          AND DATEADD(HOUR, 12, CheckIn) <= GETUTCDATE();";
+
+                    using (SqlCommand cmd = new SqlCommand(updateQuery, connection))
+                    {
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        Console.WriteLine($"{rowsAffected} user(s) auto-checked out at {DateTime.Now}");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                string toEmailIds = ConfigurationManager.AppSettings["ToEmailIds"];
+
+                EmailDetails sendEmail = new EmailDetails
+                {
+                    ToEmailIds = toEmailIds?.Split(',').Select(email => email.Trim()).ToList() ?? new List<string>(),
+                    Subject = "Daily update of Productivity_Utilization.",
+                    Body = $"Productivity_Utilization webjob failed with the following exception:  {ex.Message}",
+                };
+
+                using (HttpClient client = new HttpClient())
+                {
+                    var json = Newtonsoft.Json.JsonConvert.SerializeObject(sendEmail);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    var webApiUrl = new Uri(EmailUrl);
+                    var response = client.PostAsync(webApiUrl, content).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseData = response.Content.ReadAsStringAsync().Result;
+
+                    }
+                }
+                throw;
+            }
+
+        }
+
+    }
+}
