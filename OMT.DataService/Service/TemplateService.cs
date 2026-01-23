381490ce7dd8bc2f638d97ba11ed0c8e101ce341
Nivedhita <nivedhita.p@arxtlabs.com>
2026-01-23 11:57:59 +0530

    changed get pending order query in getorderforusder,gtetrdpendingorder,getpendingorder api's

--- OMT.DataService/Service/TemplateService.cs
+++ OMT.DataService/Service/TemplateService.cs
@@ -2773,11 +2773,11 @@ namespace OMT.DataService.Service
                                         t1.StartTime
                                     FROM 
                                         [{tablename}] AS t1
-                                    LEFT JOIN SkillSet AS t2 ON t1.SkillSetId = t2.SkillSetId
-                                    LEFT JOIN SystemOfRecord AS t3 ON t1.SystemofRecordId = t3.SystemOfRecordId
+                                    INNER JOIN SkillSet AS t2 ON t1.SkillSetId = t2.SkillSetId
+                                    INNER JOIN SystemOfRecord AS t3 ON t1.SystemofRecordId = t3.SystemOfRecordId
                                     WHERE 
                                         UserId = @UserId 
-                                        AND (Status IS NULL OR Status = '')
+                                        AND Status IS NULL 
                                     ORDER BY 
 	                                     t1.ispriority DESC,t1.StartTime ASC;";
 
