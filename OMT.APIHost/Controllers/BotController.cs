51c860f3a35a74f6865e9760d91c4a496b55bc94
Nivedhita <nivedhita.p@arxtlabs.com>
2026-02-05 20:48:24 +0530

    commented authorization in bot controller

--- OMT.APIHost/Controllers/BotController.cs
+++ OMT.APIHost/Controllers/BotController.cs
@@ -8,7 +8,7 @@ namespace OMT.APIHost.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(AuthenticationSchemes = "Bearer")]
+   // [Authorize(AuthenticationSchemes = "Bearer")]
     public class BotController : ControllerBase
     {
         private readonly IBotService _botService;
