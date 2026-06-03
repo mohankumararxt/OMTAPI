dd6dd8f7bed88c4ad9367f560bb694e6b1617850
Mohankumar S <mohankumaramtp@hotmail.com>
2026-06-03 17:55:52 +0530

    Invoice Summary

---
+++ CreateInvoiceSummary/Functions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace CreateInvoiceSummary
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, ILogger logger)
+        {
+            logger.LogInformation($"Processed queue message:{message}");
+        }
+    }
+}
