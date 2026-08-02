a338a7d3a3d1797d00c1d3268184dc3178f56bea
Mohankumar S <mohankumaramtp@hotmail.com>
2026-08-02 13:09:00 +0530

    Data Archive

--- DataArchive/Program.cs
+++ DataArchive/Program.cs
@@ -19,38 +19,90 @@ class Program
         string containerName = config["BlobStorage:ContainerName"] ?? "";
 
         // SQL connection
-        string query = "SELECT * FROM userprofile";
+        string ListQuery = "SELECT * FROM ArchiveTableList";
 
         // Fetch data
-        DataTable dt = new DataTable();
+        DataTable ArchiveListdt = new DataTable();
         
         using (SqlConnection conn = new SqlConnection(sqlConnString))
         {
-            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
-            adapter.Fill(dt);
+            SqlDataAdapter adapter = new SqlDataAdapter(ListQuery, conn);
+            adapter.Fill(ArchiveListdt);
         }
 
-        // Create Excel workbook
-        using (XLWorkbook wb = new XLWorkbook())
+        foreach(DataRow row in ArchiveListdt.Rows)
         {
-            wb.Worksheets.Add(dt, "Sheet1");
+            Console.WriteLine("Table Name : " + row["TableName"].ToString() +", Column Name : " + row["ColumnName"].ToString());
 
-            using (var stream = new MemoryStream())
+            DateTime currentDate = DateTime.Now;
+            DateTime threeMonthsAgo = currentDate.AddMonths(-3);
+
+            int monthValue = threeMonthsAgo.Month; // Gives the month number (1–12)
+            string monthName = threeMonthsAgo.ToString("MMMM"); // Gives the full month name
+            string Year = threeMonthsAgo.ToString("yyyy"); // Gives the year
+
+            Console.WriteLine("Month -3 (number): " + monthValue);
+            Console.WriteLine("Month -3 (name): " + monthName);
+
+            DataTable TableData = new DataTable();
+            string TableDataListQuery = "SELECT * FROM "+ row["TableName"].ToString() +" Where " + row["ColumnName"].ToString() +" < '" + threeMonthsAgo +"'";
+
+            using (SqlConnection conn = new SqlConnection(sqlConnString))
             {
-                wb.SaveAs(stream);
-                stream.Position = 0;
-
                 // Upload to Azure Blob Storage
-                string blobName = "YourTableData.xlsx";
+                string blobName = Year +"/" + monthName + "/" + row["TableName"].ToString()+"_" + monthName + ".xlsx";
 
-                BlobContainerClient container = new BlobContainerClient(blobConnString, containerName);
-                container.CreateIfNotExists();
+                SqlDataAdapter adapter = new SqlDataAdapter(TableDataListQuery, conn);
+                adapter.Fill(TableData);
 
-                BlobClient blob = container.GetBlobClient(blobName);
-                blob.Upload(stream, overwrite: true);
+                // Create Excel workbook
+                using (XLWorkbook wb = new XLWorkbook())
+                {
+                    wb.Worksheets.Add(TableData, "Sheet1");
 
-                Console.WriteLine("Excel file uploaded to Blob Storage: " + blob.Uri);
+                    using (var stream = new MemoryStream())
+                    {
+                        wb.SaveAs(stream);
+                        stream.Position = 0;
+
+                        
+
+                        BlobContainerClient container = new BlobContainerClient(blobConnString, containerName);
+                        container.CreateIfNotExists();
+
+                        BlobClient blob = container.GetBlobClient(blobName);
+                        blob.Upload(stream, overwrite: true);
+
+                        Console.WriteLine("Excel file uploaded to Blob Storage: " + blob.Uri);
+                    }
+                }
+
+                Console.WriteLine(blobName + " Created Successfully.");
             }
+
         }
+
+        //// Create Excel workbook
+        //using (XLWorkbook wb = new XLWorkbook())
+        //{
+        //    wb.Worksheets.Add(dt, "Sheet1");
+
+        //    using (var stream = new MemoryStream())
+        //    {
+        //        wb.SaveAs(stream);
+        //        stream.Position = 0;
+
+        //        // Upload to Azure Blob Storage
+        //        string blobName = "YourTableData.xlsx";
+
+        //        BlobContainerClient container = new BlobContainerClient(blobConnString, containerName);
+        //        container.CreateIfNotExists();
+
+        //        BlobClient blob = container.GetBlobClient(blobName);
+        //        blob.Upload(stream, overwrite: true);
+
+        //        Console.WriteLine("Excel file uploaded to Blob Storage: " + blob.Uri);
+        //    }
+        //}
     }
 }
